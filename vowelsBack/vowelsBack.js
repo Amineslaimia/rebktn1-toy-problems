0fc2b8007c6bb97958db58c7e35d7511f7d1801e
amineslaimia <medamineslaimia@gmail.com>
2019-12-06 09:49:35 +0100

    message

--- vowelsBack/vowelsBack.js
+++ vowelsBack/vowelsBack.js
@@ -11,3 +11,103 @@
 // If a moved letter becomes 'c', 'o', 'd' or 'e', revert it back to it's original value.
 
 // Provided string will always be lower case, won't be empty and will have no special characters.
+
+
+
+
+
+function vowelsBack(string){
+    var alphabet='abcdefghijklmnopqrstuvwxyz';
+    var vowels='aiu';
+    var consonants='bfghjklmnpqrstvwxyz';
+    var moveback='code'
+    var result =''
+   for (var i =0 ; i <string.length;i++){
+       if(consonants.indexOf(string[i])!== -1){
+           if(alphabet.indexOf(string[i])-9<0){
+               if(moveback.indexOf(alphabet[25+(alphabet.indexOf(string[i])-9)])!== -1){
+                result=result+string[i]
+               }else{
+                result=result+alphabet[25+(alphabet.indexOf(string[i])-9)]
+               }
+           }else{
+            if(moveback.indexOf(alphabet[alphabet.indexOf(string[i])-9])!== -1){
+                result=result+string[i]
+               }else{
+                result=result+alphabet[alphabet.indexOf(string[i])-9]
+               
+               }
+               
+           }
+           
+   
+       }else if(vowels.indexOf(string[i])!== -1){
+        if(alphabet.indexOf(string[i])-5<0){
+            if(moveback.indexOf(alphabet[25+(alphabet.indexOf(string[i])-5)])!== -1){
+             result=result+string[i]
+            }else{
+             result=result+alphabet[25+(alphabet.indexOf(string[i])-5)]
+            }
+        }else{
+         if(moveback.indexOf(alphabet[alphabet.indexOf(string[i])-5])!== -1){
+             result=result+string[i]
+            }else{
+             result=result+alphabet[alphabet.indexOf(string[i])-5]
+            
+            }
+            
+        }
+       }else if(string[i]==='c'||string[i]==='o'){
+        if(alphabet.indexOf(string[i])-1<0){
+            if(moveback.indexOf(alphabet[25+(alphabet.indexOf(string[i])-1)])!== -1){
+             result=result+string[i]
+            }else{
+             result=result+alphabet[25+(alphabet.indexOf(string[i])-1)]
+            }
+        }else{
+         if(moveback.indexOf(alphabet[alphabet.indexOf(string[i])-1])!== -1){
+             result=result+string[i]
+            }else{
+             result=result+alphabet[alphabet.indexOf(string[i])-1]
+            
+            }
+            
+        }
+       }else if(string[i]==='d'){
+        if(alphabet.indexOf(string[i])-3<0){
+            if(moveback.indexOf(alphabet[25+(alphabet.indexOf(string[i])-3)])!== -1){
+             result=result+string[i]
+            }else{
+             result=result+alphabet[25+(alphabet.indexOf(string[i])-3)]
+            }
+        }else{
+         if(moveback.indexOf(alphabet[alphabet.indexOf(string[i])-3])!== -1){
+             result=result+string[i]
+            }else{
+             result=result+alphabet[alphabet.indexOf(string[i])-3]
+            
+            }
+            
+        }
+       }else if(string[i]==='e'){
+        if(alphabet.indexOf(string[i])-4<0){
+            if(moveback.indexOf(alphabet[25+(alphabet.indexOf(string[i])-4)])!== -1){
+             result=result+string[i]
+            }else{
+             result=result+alphabet[25+(alphabet.indexOf(string[i])-4)]
+            }
+        }else{
+         if(moveback.indexOf(alphabet[alphabet.indexOf(string[i])-4])!== -1){
+             result=result+string[i]
+            }else{
+             result=result+alphabet[alphabet.indexOf(string[i])-4]
+            
+            }
+            
+        }
+       }
+       
+   }
+   return  result
+   }
+ 
