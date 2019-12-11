335be7a60fd6434276857083f77e8b824bd40608
amineslaimia <medamineslaimia@gmail.com>
2019-12-11 09:29:20 +0100

    toy tree merge

--- mergeTwoBinaryTrees/merge.js
+++ mergeTwoBinaryTrees/merge.js
@@ -36,4 +36,16 @@ Note: The merging process must start from the root nodes of both trees.
  * @param {TreeNode} t2
  * @return {TreeNode}
  */
-var mergeTrees = function(t1, t2) {};
+var mergeTrees = function(t1, t2) {
+
+   
+	if (t1 === null)
+		return t2;
+	if (t2 ===null)
+		return t1;
+	t1.val += t2.val;
+	t1.left = mergeTrees(t1.left, t2.left);
+	t1.right = mergeTrees(t1.right, t2.right);
+	return t1;
+
+}
