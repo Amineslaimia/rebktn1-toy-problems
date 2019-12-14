4492efe3da8ffa806dcc0c63bf025f8e7d4899a5
amineslaimia <medamineslaimia@gmail.com>
2019-12-14 09:25:50 +0100

    done bad time complexity

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -7,4 +7,21 @@ largestProductOfThree([ 0, 2, 3 ]) //	0
 largestProductOfThree([1, 4, -5, -2]) // 40
 */
 
-function largestProductOfThree(array) {}
+function largestProductOfThree(array) {
+    var product =0;
+    for (var i =0 ; i<array.length-2;i++){
+        for(var j=i+1 ;j<array.length-1;j++){
+            for(var k = j+1 ;k<array.length;k++){
+                
+                if(array[i]!==array[j]!==array[k]){
+                if(product<array[i]*array[j]*array[k]){
+                    product=array[i]*array[j]*array[k];
+
+                }
+                }
+            }
+        }
+    }
+    return product
+}
+
