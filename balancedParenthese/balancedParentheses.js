1804df07d66fa5a9ca5e5a79bc6f5fc0255af5f5
amineslaimia <medamineslaimia@gmail.com>
2019-12-20 09:07:24 +0100

    done

--- balancedParenthese/balancedParentheses.js
+++ balancedParenthese/balancedParentheses.js
@@ -8,4 +8,15 @@ isBalanced("(50)(")	// false
 isBalanced("") //	true
 */
 
-var isBalanced = function(str) {};
+var isBalanced = function(str) {
+    var closecount=0
+    var opencount =0
+    for (var i = 0 ; i <str.length;i++){
+        if(str[i]==='('){
+            opencount++
+        }else if(str[i]===')'){
+            closecount++
+        }
+    }
+    return closecount===opencount
+};
