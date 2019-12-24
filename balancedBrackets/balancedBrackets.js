ad7e58843c647583520012ebb98fd414a4c3908a
amineslaimia <medamineslaimia@gmail.com>
2019-12-24 09:13:38 +0100

    message

--- balancedBrackets/balancedBrackets.js
+++ balancedBrackets/balancedBrackets.js
@@ -9,4 +9,28 @@ isBalanced("(50)(") // false
 isBalanced("[{]}") // false
 */
 
-var isBalanced = function(str) {};
+var isBalanced = function(str) {
+    var  paraesCount =0;
+    var  curlyCount =0;
+    var  squareCount =0;
+    
+      for (var i = 0 ;i <str.length; i++){
+          if(str[i]==='(')
+          paraesCount++;
+          if(str[i]===')')
+          paraesCount--;
+          if(str[i]==='{')
+          curlyCount++;
+          if(str[i]==='}')
+          curlyCount--;
+          if(str[i]==='[')
+          squareCount++;
+          if(str[i]===']')
+          squareCount--;
+          console.log(paraesCount,curlyCount =0,squareCount)
+          if(paraesCount <0 ||curlyCount<0 ||squareCount<0 ){
+              return false
+          }
+      } 
+      return(paraesCount===0 &&curlyCount===0 &&squareCount===0 )
+  };
