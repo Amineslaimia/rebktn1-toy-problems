760d1c2757eda534f9915deb6fe251aef7f6b3b1
amineslaimia <medamineslaimia@gmail.com>
2019-12-26 09:32:52 +0100

    done O(n)

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,25 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
+ var allAnagrams = function(string) {
   // Your code here.
+var length=string.length**string.length
+var obj={}
+var i =0;
+var result =[]
+while(i<length){
+  var str = "";
+  while(str.length < string.length){
+    str += string[Math.floor(Math.random() * string.length)];
+  }
+  if(obj[str]===undefined){
+    obj[str] =null 
+    i++
+  }
+}
+return Object.keys(obj)
+
 };
+
+
+
