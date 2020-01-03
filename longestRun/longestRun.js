fd19c609e8cf1da81b13bbad9867246d8e1f210a
amineslaimia <medamineslaimia@gmail.com>
2020-01-03 09:34:08 +0100

    done

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -12,4 +12,35 @@ longestRun("abcdefgh") // [ 0, 0 ]
 longestRun("abccccccc") // [ 2, 8 ]
 */
 
-function longestRun(string) {}
+function longestRun(string) {
+    var longestIndexStart = -1;
+         var longestCount = 0;
+         var currentCount = 0;
+         var currentIndexStart = 0;
+         if(string===''){
+                 return [0,0]
+         }
+ 
+         for (var idx = 0; idx < string.length; idx++){
+                 
+             if (string[idx] === string[currentIndexStart]){
+                 currentCount++;
+                 if(idx===string.length-1){
+                     longestIndexStart = currentIndexStart;
+                     longestCount = currentCount;
+                         break
+                 }
+                 
+             }else{
+                 if (currentCount > longestCount)
+                 {
+                     longestIndexStart = currentIndexStart;
+                     longestCount = currentCount;
+                 }
+ 
+                 currentIndexStart = idx;
+                 currentCount = 1;
+             }
+         }
+         return [longestIndexStart,longestIndexStart+longestCount-1]
+ }
