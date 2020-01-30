4672e3c53114d548e1779f181a7eafa8c936d393
amineslaimia <medamineslaimia>
2020-01-30 09:42:13 +0100

    done

--- sortedInsert/sortedInsert.js
+++ sortedInsert/sortedInsert.js
@@ -17,7 +17,7 @@ The stack implementation is available to view below.
 
 // the stack class is exposed here for reference, you may or may not utilize additional stacks in your algorithm.
 
-var Stack = function(initialValue) {
+var Stack = function (initialValue) {
   this.store = {};
   this.count = 0;
 
@@ -26,19 +26,19 @@ var Stack = function(initialValue) {
   }
 };
 
-Stack.prototype.isEmpty = function() {
+Stack.prototype.isEmpty = function () {
   return !this.count;
 };
 
-Stack.prototype.peek = function() {
+Stack.prototype.peek = function () {
   return this.store[this.count];
 };
 
-Stack.prototype.push = function(val) {
+Stack.prototype.push = function (val) {
   this.store[++this.count] = val;
 };
 
-Stack.prototype.pop = function() {
+Stack.prototype.pop = function () {
   if (!this.isEmpty()) {
     var tmp = this.store[this.count];
     delete this.store[this.count--];
@@ -48,4 +48,27 @@ Stack.prototype.pop = function() {
 
 function sortedInsert(stack, element) {
   // your code here...
+  var index = 0
+  if (stack.store[stack.count] > element) {
+    stack.push(element)
+  }
+  else if (stack.store[1] < element) {
+    stack.count++
+    for (var i = stack.count; i < 1; i--) {
+      stack.store[i] = stack.store[i - 1]
+    }
+    stack.store[1] = element
+  } else {
+    for (var i = stack.count - 1; i < 0; i--) {
+      if (stack.store[i] > element) {
+        index = i
+      }
+    }
+    stack.count++
+    for (var i = stack.count; i < index; i--) {
+      stack.store[i] = stack.store[i - 1]
+    }
+    stack.store[index - 1] = element
+  }
+  return stack
 }
